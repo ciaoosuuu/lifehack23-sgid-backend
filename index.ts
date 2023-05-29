b2386b7efeeca089733113e49c2c63d501d07724
ciaoosuuu <>
2023-05-30 06:03:25 +0800

    edit frontend host

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ const redirectUri = String(
   process.env.SGID_REDIRECT_URI ?? `https://vccm-sgid.onrender.com/api/redirect`
 );
 const frontendHost = String(
-  process.env.SGID_FRONTEND_HOST ?? "https://cors-anywhere.herokuapp.com/"
+  process.env.SGID_FRONTEND_HOST ?? "https://vccm.netlify.app"
 );
 
 const sgid = new SgidClient({
